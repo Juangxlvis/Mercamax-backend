3cb89b5a91a5f262ae763b6dc06a3ea5466d4497
Juangxlvis <galvis2044@gmail.com>
2026-03-05 20:59:04 -0500

    fix: quitar api de la url de listar roles

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from .views import InviteUserView, RolListView, ActivateAccountView, ValidateTok
 
 urlpatterns = [
     path('admin/invite/', InviteUserView.as_view(), name='invite-user'),
-    path('api/roles/', RolListView.as_view(), name='roles-list'),
+    path('roles/', RolListView.as_view(), name='roles-list'),
     path('complete-registration/', ActivateAccountView.as_view(), name='complete-registration'),
     path('password/reset/', ForgotPasswordView.as_view(), name='password_reset'),
     path('password/reset/confirm/<uidb64>/<token>/', ResetPasswordConfirmView.as_view(), name='password_reset_confirm'),    
