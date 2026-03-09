f0b35b783cb894accc2db316d64f8c1674a8f512
Juangxlvis <galvis2044@gmail.com>
2026-03-08 19:54:05 -0500

    fix: envío de codigo 2fa al email, de nuevo, cambio de TLS A SSL

--- mercamax/settings.py
+++ mercamax/settings.py
@@ -172,15 +172,20 @@ CORS_ALLOWED_ORIGINS = [
 
 SITE_ID = 1
 
-# Configuración de Email para desarrollo
-EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
+# Configuración de Email para producción
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+
+# Cambiamos el puerto por defecto a 465
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465)) 
+
+# Apagamos TLS por defecto y encendemos SSL
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True" # ¡ESTA LÍNEA ES LA MAGIA!
+
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
-
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
