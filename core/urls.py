d70240279f82192e86089157cc8690d8479ae896
Juangxlvis <galvis2044@gmail.com>
2026-03-09 15:34:12 -0500

    Fix: notificaciones, marcar todas leidas

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 # core/urls.py
 from .views import NotificacionListView
 from django.urls import path
-from .views import NotificacionListView
+from .views import NotificacionListView, MarcarTodasLeidasView
 
 urlpatterns = [
     path('notificaciones/', NotificacionListView.as_view(), name='lista-notificaciones'),
+    path('notificaciones/marcar-todas-leidas/', MarcarTodasLeidasView.as_view(), name='marcar-leidas'),
 ]
 
